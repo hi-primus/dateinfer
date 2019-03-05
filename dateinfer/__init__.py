77f681b5a61e7479d78790ca524d530463956254
Jan Trienes <jan.trienes@googlemail.com>
2019-03-05 11:56:07 +0100

    Bump add __version__ to package, bump version and remove dist/ folder

--- dateinfer/__init__.py
+++ dateinfer/__init__.py
@@ -1,3 +1,4 @@
 __author__ = 'jeffrey.starr@ztoztechnologies.com'
+__version__ = "0.4.0"
 
 from dateinfer.infer import infer
--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
+import re
 from distutils.core import setup
 
+
+def get_version(version_file):
+    pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    match = re.search(pattern, open(version_file, "rt").read(), re.MULTILINE)
+    if match:
+        return match.group(1)
+
+    raise RuntimeError("Unable to find version string in %s." % (version_file,))
+
+
 setup(name='pydateinfer',
-      version='0.3.0',
+      version=get_version("dateinfer/__init__.py"),
       description='Infers date format from examples',
       long_description="""Uses a series of pattern matching and rewriting rules to compute a "best guess" datetime.strptime format string give a list of example date strings.""",
       author='Jeffrey Starr',
       author_email='will@pedalwrencher.com',
-      url='https://github.com/wdm0006/dateinfer',
+      url='https://github.com/nedap/dateinfer',
       packages=['dateinfer'],
       classifiers=[
           'Development Status :: 4 - Beta',
@@ -14,7 +25,6 @@ setup(name='pydateinfer',
           'License :: OSI Approved :: Apache Software License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],
